e620a43bdaf82d4fe68691a33996aead7bee133b
hszew <h.szewczyk9@gmail.com>
2026-05-30 14:43:13 +0200

    add leaderboard aggregation to home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,24 +1,43 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MLOps_Dashboard.Models;
+using System.Diagnostics;
 
-namespace MLOps_Dashboard.Controllers;
-
-public class HomeController : Controller
+namespace MLOps_Dashboard.Controllers
 {
-    public IActionResult Index()
+    public class HomeController : Controller
     {
-        return View();
-    }
+        private readonly MLOpsContext _context;
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        // Inject the database context
+        public HomeController(MLOpsContext context)
+        {
+            _context = context;
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Index()
+        {
+            // Group by Dataset, and find the TrainingRun with the highest Accuracy in each group.
+            var bestRuns = _context.TrainingRuns
+                .Include(t => t.Dataset)
+                .Include(t => t.Architecture)
+                .ToList() // Evaluate client-side since SQLite has limited GroupBy support
+                .GroupBy(t => t.Dataset.Name)
+                .Select(group => group.OrderByDescending(t => t.Accuracy).First())
+                .ToList();
+
+            return View(bestRuns);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
